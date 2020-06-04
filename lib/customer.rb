1b7e72023e1e7d132c7883f2a748e2ae7dced4d0
ameerrah9 <mccullers.ghameerah@gmail.com>
2020-06-04 12:51:54 -0400

    Done.

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,3 +1,52 @@
 class Customer
   
+  attr_accessor :name, :age
+  attr_reader
+  attr_writer
+
+    # We want to make sure when building out classes, that there's something to store each instance.
+    # The class should know about each class instance that gets created, we store those in @@all
+    @@all = []
+
+    def initialize(name, age)
+        @name = name
+        @age = age
+        @@all << self
+    end
+     
+    def self.all
+        @@all
+    end
+     
+    # create a meal as a customer
+    # associates the meal with the customer(self) that created it
+    def new_meal(waiter, total, tip=0)
+        Meal.new(waiter, self, total, tip)
+    end
+
+    # we need a way for our customer and waiter instances to 
+    # get information about each other.
+    # we can get info about waiter through the meals they've created
+    # a customer might want to know the name of the waiter 
+    # the customer is going to look at all of the meals, 
+    # and then select only the ones that belong to them.
+
+    def meals
+        Meal.all.select {|meal| meal.customer == self}    
+    end
+    # a way for a customer to know about all of their meals
+    # this method iterates through every instance of the Meal class
+    # returns only the ones where the meal's customer matches the current customer
+    # this method only returns the meal the customer is associated with.
+
+    # this method will return all the waiters associated with the customer
+    # gets every waiter from every meal a customer created
+    # gets the waiter from each of the meals
+    def waiters
+        meals.map {|meal| meal.waiter}
+    end
+
+    # find the name of the customer's last waiter
+    # customer.waiters.last.name
+
 end
--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,3 +1,47 @@
 class Waiter
+ 
+  attr_accessor :name, :yrs_experience
+   
+    @@all = []
+   
+    def initialize(name, yrs_experience)
+      @name = name
+      @yrs_experience = yrs_experience
+      @@all << self
+    end
+   
+    def self.all
+      @@all
+    end
 
-end
+    # create a meal
+    def new_meal(customer, total, tip=0)
+        Meal.new(self, customer, total, tip)
+    end
+
+    # we need a way for our customer and waiter instances to 
+    # get information about each other.
+    # we can get info about customer through the meals they've created
+    # a waiter might want to know who their regular customers are
+    # and what meals those customers usually order
+    # a method so a waiter can find a customer
+    def meals
+        Meal.all.select do |meal|
+          meal.waiter == self #checking for waiter now
+        end
+    end
+    # a way for a waiter to find all the meals they created
+
+    def best_tipper
+        best_tipped_meal = meals.max do |meal_a, meal_b|
+          meal_a.tip <=> meal_b.tip
+        end
+       
+        best_tipped_meal.customer
+    end
+   
+  end
+
+  # a customer can have many meals.
+  # different instances of the meal
+  # relationship with the customer is through your waiter.
