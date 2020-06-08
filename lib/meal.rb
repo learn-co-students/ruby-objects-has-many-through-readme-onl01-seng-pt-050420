7d324f83c1b7dee66f84ed11b60517265a4cecee
Joshua Williams <jmw0426@hotmail.com>
2020-06-08 19:04:39 +0000

    Done.

--- lib/meal.rb
+++ lib/meal.rb
@@ -1,3 +1,25 @@
 class Meal
+  attr_accessor :waiter, :customer, :total, :tip 
 
+@@all =[]
+
+  def initialize(waiter, customer, total, tip=0)
+    @waiter = waiter
+    @customer = customer
+    @total = total 
+    @tip = tip 
+    @@all << self 
+  end 
+  
+  def self.all 
+    @@all
+  end
+  
+  def gross_income
+    self.all.total.each { |total| total + total }
+  end
+  
+  def all_tips
+    self.all.tip.each { |tips| tips + tips }
+  end
 end
--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,3 +1,50 @@
 class Waiter
-
+  attr_accessor :name, :yrs_experience
+  
+  @@all =[]
+  
+  def initialize(name, yrs_experience)
+    @name = name 
+    @yrs_experience = yrs_experience
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def new_meal(customer, total, tip=0)
+    Meal.new(self, customer, total, tip)
+  end 
+  
+  def meals
+    Meal.all.select { |meal| meal.waiter == self }
+  end 
+  
+  def best_tipper
+    best_tip = meals.max { |a, b| a.tip <=> b.tip }
+    best_tip.customer
+  end 
+  
+  def worst_tipper
+    worst_tip = meals.min { |a, b| a.tip <=> b.tip }
+    worst_tip.customer
+  end 
+  
+  def most_frequent_customer
+    Customer.all.select { |patron| patron.waiter == self } 
+  end 
+  
+  def tips
+    Meal.all.select { |waiter| waiter.tip == self }
+  end
+  
+  def most_tips
+    highest_tips = tips.max { |a, b| a.tip <=> b.tip }
+    highest_tips.waiter
+  end
+  
+  def most_experienced
+    self.all.yrs_experience.sort
+  end
 end
